768ab6cba5c374763e746ef78145d22086fa89cc
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-10-28 13:15:13 -0700

    moving guy

--- src/prefabs/play.js
+++ src/prefabs/play.js
@@ -5,7 +5,30 @@ class Play extends Phaser.Scene {
   preload() {}
 
   create() {
-    this.add.sprite(100, 100, "mushroomBomb");
+    //this.add.sprite(100, 100, "mushroomBomb");
+    this.keys = this.input.keyboard.createCursorKeys();
+    this.keys = this.input.keyboard.addKeys({
+      up: "W",
+      left: "A",
+      down: "S",
+      right: "D",
+    });
+    this.player = new Player(this, 100, 100, "mushroomBomb");
   }
-  update() {}
+  update() {
+    const { left, right, up, down } = this.keys;
+    if (left.isDown) {
+      this.player.setVelocityX(-100);
+    } else if (right.isDown) {
+      this.player.setVelocityX(100);
+    }
+    if (up.isDown) {
+      this.player.setVelocityY(-100);
+    } else if (down.isDown) {
+      this.player.setVelocityY(100);
+    }
+    if (!left.isDown && !down.isDown && !up.isDown && !right.isDown) {
+      this.player.setVelocity(0);
+    }
+  }
 }
---
+++ src/prefabs/player.js
@@ -0,0 +1,75 @@
+class Player extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, texture) {
+    super(scene, x, y, texture); // call Sprite parent class
+    scene.add.existing(this); // add Player to existing scene
+    scene.physics.add.existing(this); // add physics body to scene
+
+    //set player values
+    this.direction = new Phaser.Math.Vector2(0);
+    this.velocityS = 100;
+  }
+}
+class IdleState extends State {
+  //Player idle state
+  enter(scene, hero) {
+    if (scene.input.keyboard.enabled == true) {
+      hero.body.setVelocity(0);
+    }
+  }
+
+  execute(scene, hero) {
+    const { left, right, up, down } = scene.keys;
+    //transition to new states
+    if (left.isDown || right.isDown || up.isDown || down.isDown) {
+      this.stateMachine.transition("move");
+      return;
+    }
+
+    /*scene.input.on('pointerdown', function (pointer)
+        {
+            this.stateMachine.transition('idleSwing');
+        }, this);*/
+  }
+}
+
+class MoveState extends State {
+  execute(scene, hero) {
+    const { left, right, up, down } = scene.keys;
+
+    let direction = new Phaser.Math.Vector2(0);
+    //Character movement
+    if (!(left.isDown || right.isDown || up.isDown || down.isDown)) {
+      this.stateMachine.transition("idle");
+      return;
+    }
+
+    if (up.isDown || down.isDown) {
+      if (up.isDown) {
+        //hero.setFlip(true, false);
+        hero.direction.y = -1;
+      } else if (down.isDown) {
+        hero.direction.y = 1;
+      }
+      hero.direction.normalize();
+      hero.body.setVelocityY(hero.velocityS * hero.direction.y);
+    } else {
+      hero.setVelocityY(0);
+      hero.direction.y = 0;
+    }
+
+    if (left.isDown || right.isDown) {
+      if (left.isDown) {
+        hero.setFlip(true, false);
+        hero.direction.x = -1;
+      } else if (right.isDown) {
+        hero.resetFlip();
+        hero.direction.x = 1;
+      }
+      hero.direction.normalize();
+      hero.body.setVelocityX(hero.velocityS * hero.direction.x);
+    } else {
+      hero.setVelocityX(0);
+      hero.direction.x = 0;
+    }
+  }
+}
