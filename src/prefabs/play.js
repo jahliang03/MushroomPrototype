2c9be41f6e17c225ec853a503e32b67f7bdd55b0
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-10-31 13:38:50 -0700

    Enemy Idle Movement, expanded worldbounds

--- src/prefabs/play.js
+++ src/prefabs/play.js
@@ -3,10 +3,11 @@ class Play extends Phaser.Scene {
     super("playScene");
   }
 
-  preload() {
-  }
+  preload() {}
 
   create() {
+    this.worldBoundX = 2000;
+    this.worldBoundY = 2000;
     this.keys = this.input.keyboard.addKeys({
       up: "W",
       left: "A",
@@ -19,11 +20,16 @@ class Play extends Phaser.Scene {
 
     this.mobs = this.add.group(); // Creating group to add all mobs
     addMob(this.mobs, this);
-    this.testMobDead = false;
+
+    this.enemCount = 0;
 
     this.player = new Player(this, 100, 100, "mushroomPlayer");
-    this.player.setScale(0.3);
+    this.player.setScale(0.2);
     this.physics.add.existing(this.player);
+
+    this.cameras.main.setBounds(0, 0, this.worldBoundX, this.worldBoundY);
+    this.cameras.main.startFollow(this.player, true, 0.25, 0.25);
+    this.physics.world.setBounds(0, 0, this.worldBoundX, this.worldBoundY);
 
     // Player state machine initialization
     this.playerFSM = new StateMachine(
@@ -62,18 +68,24 @@ class Play extends Phaser.Scene {
     if (!left.isDown && !down.isDown && !up.isDown && !right.isDown) {
       this.player.setVelocity(0);
     }
-    if (this.testMobDead) {
+    while (this.enemCount < 20) {
       addMob(this.mobs, this);
-      this.testMobDead = false;
+      this.enemCount++;
     }
     mobMovement(this.mobs, this); // Checks which mobs need to move
   }
 }
 
 function mobMovement(mobList, scene) {
+  if (mobList == null) {
+    return;
+  }
   mobList.children.each(function (enemy) {
     // Only move if the enemy has been hit
-    if (enemy.hit && Phaser.Math.Distance.BetweenPoints(enemy, scene.player) < 300) {
+    if (
+      enemy.hit &&
+      Phaser.Math.Distance.BetweenPoints(enemy, scene.player) < 300
+    ) {
       if (scene.player.x < enemy.x && enemy.body.velocity.x >= 0) {
         enemy.setVelocityX(-enemy.speed);
       } else if (scene.player.x > enemy.x && enemy.body.velocity.x <= 0) {
@@ -86,17 +98,38 @@ function mobMovement(mobList, scene) {
         enemy.setVelocityY(enemy.speed);
       }
     } else {
-      enemy.setVelocity(0);
+      if (enemy.toggleIdle) {
+        //Idle movement for animals
+        let direcY = Math.floor(Math.random() * 2 + 1);
+        if (direcY % 2 == 0) {
+          direcY = -1;
+        }
+        let direcX = Math.floor(Math.random() * 2 + 1);
+        if (direcX % 2 == 0) {
+          direcX = -1;
+        }
+        if (Math.floor(Math.random() * 3) == 1) {
+          enemy.setVelocity(0);
+        } else {
+          enemy.setVelocity(enemy.speed * direcX, enemy.speed * direcY);
+        }
+        enemy.toggleIdle = false;
+        scene.time.delayedCall(1000, idleToggle, [enemy], scene);
+      }
     }
     enemy.healthText.setPosition(enemy.x, enemy.y - 20);
   }, scene);
 }
 
+function idleToggle(object) {
+  object.toggleIdle = true;
+}
+
 function addMob(mobGroup, scene) {
   // Adds enemy to given group
   let enem = scene.physics.add.sprite(
-    Math.random() * (config.width - 200) + 100,
-    Math.random() * (config.height - 200) + 100,
+    Math.random() * (scene.worldBoundX - 100),
+    Math.random() * (scene.worldBoundY - 100),
     "mushroomBomb" // Enemy texture
   );
   enem.body.setCollideWorldBounds(true);
@@ -104,11 +137,14 @@ function addMob(mobGroup, scene) {
   enem.speed = 50;
   enem.health = 5;
   enem.hit = false;
+  enem.toggleIdle = true;
 
-  enem.healthText = scene.add.text(enem.x, enem.y - 20, enem.health, {
-    font: "16px Arial",
-    fill: "#ff0000",
-  }).setOrigin(0.5);
+  enem.healthText = scene.add
+    .text(enem.x, enem.y - 20, enem.health, {
+      font: "16px Arial",
+      fill: "#ff0000",
+    })
+    .setOrigin(0.5);
 
   // Override preUpdate to make the text follow the enemy
   enem.preUpdate = function (time, delta) {
